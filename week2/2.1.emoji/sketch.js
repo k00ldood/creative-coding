670d4220956f97da3dc827f68ea00a4a6fc8cb8c
k00ldood <bbcharlieca@gmail.com>
2023-05-22 17:31:06 -0400

    Update sketch.js

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -1,7 +1,32 @@
 function setup() {
   createCanvas(400, 400);
 }
-
+  //outline of the face
 function draw() {
   background(220);
+  stroke("black");
+  strokeWeight(5);
+  
+  //overall face
+  fill("yellow");
+  ellipse(200,200,250,250);
+  
+  //eyebrows
+  fill("black");
+  ellipse(145,155,60,0);
+  ellipse(255,155,60,0);
+
+  //both eyes left then right
+  fill("black");
+  ellipse(150,175,40,40);
+  ellipse(250,175,40,40);
+  
+  //inner eye
+  fill("white");
+  ellipse(150,175,25,25);
+  ellipse(250,175,25,25);
+  
+  //mouth
+  fill("black");
+  ellipse(200,260,50,50);
 }
