4d76a4eaa8d073f952373edae0457e47d62bf1e1
k00ldood <bbcharlieca@gmail.com>
2023-05-23 16:38:48 -0400

    Update sketch.js

--- week2/2.2.circles/sketch.js
+++ week2/2.2.circles/sketch.js
@@ -1,7 +1,35 @@
 function setup() {
   createCanvas(1200, 1200);
+  noLoop();
 }
 
 function draw() {
   background(220);
+
+//pushing the grid out of the top left corner
+translate(150,160)
+
+for (let x = 0; x < 10; x += 1){
+   for (let y = 0; y < 10; y+= 1){
+    push();
+      
+      //move the drawing plan to
+      //add a new circle
+      translate(x * 100,y * 100);
+
+      //line thickness
+      strokeWeight( random(4,40) );
+
+      //line color
+      stroke(random(0,255),184,255);
+
+      //fill color
+      fill(0,random(0,255),160);
+
+      //draw the circle
+      ellipse(0,0,random(75,130));
+
+      pop();  
+    }
+  }
 }
