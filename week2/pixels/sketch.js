941680a8a9bb25b6b1da27dcc6cb72dcfaf93375
k00ldood <bbcharlieca@gmail.com>
2023-05-28 20:31:04 -0400

    Update sketch.js

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,7 +1,13 @@
+let img;
+function preload(){
+   img = loadImage('https://k00ldood.github.io/creative-coding/week2/pixels/22203-diplopia.jpg');
+}
 function setup() {
   createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
+
+  image(img, 0, 0); 
 }
