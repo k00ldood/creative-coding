23a10cad1be5692a888606dff8629d8ca27e69d0
k00ldood <bbcharlieca@gmail.com>
2023-05-31 20:16:37 -0400

    Update sketch.js

--- week3/3.2.follower/sketch.js
+++ week3/3.2.follower/sketch.js
@@ -1,12 +1,12 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
 let speedfactor = 3;
 let xspeed = speedfactor;
 let yspeed = speedfactor;
 let x = 0;
 let y = 0;
-
-function setup() {
-  createCanvas(800, 800);
-}
 
 function draw() {
   background(220);
