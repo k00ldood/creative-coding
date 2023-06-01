191544ffa328d9edb384876b1410b368f36ff4c5
k00ldood <bbcharlieca@gmail.com>
2023-05-31 20:32:29 -0400

    testing

--- week3/3.2.follower/sketch.js
+++ week3/3.2.follower/sketch.js
@@ -1,34 +1,46 @@
-function setup() {
-  createCanvas(800, 800);
-}
-
 let speedfactor = 3;
 let xspeed = speedfactor;
 let yspeed = speedfactor;
 let x = 0;
 let y = 0;
 
+let debug = true;
+let followerImage;
+
+function preload() {
+  followerImage = loadImage("download.png");
+}
+
+function setup() {
+  createCanvas(800, 800);
+}
+
 function draw() {
   background(220);
-  
-  // Update the position of the object based on the mouse pointer
-  x += (mouseX - x) / 10;
-  y += (mouseY - y) / 10;
 
-  // Drawing the object
-  // Replace the ellipse() function with an image
-  let imgSize = 50; 
+  if (mouseX > x) {
+    xspeed = speedfactor;
+  } else {
+    xspeed = -speedfactor;
+  }
+
+  if (mouseY > y) {
+    yspeed = speedfactor;
+  } else {
+    yspeed = -speedfactor;
+  }
+
+  x += xspeed;
+  y += yspeed;
+
+  let imgSize = 50;
   imageMode(CENTER);
-  image(caillou.jpg, x, y, imgSize, imgSize); 
+  image(followerImage, x, y, imgSize, imgSize);
 
-  // Calculate the distance between the object and the mouse pointer
-  let distance = dist(x, y, mouseX, mouseY);
-
-  // Check if the distance is small enough to register as a catch
-  let catchDistance = 50; // Adjust the catch distance as needed
-  if (distance < catchDistance) {
-    // Reset the position of the object after a successful catch
-    x = random(width);
-    y = random(height);
+  if (debug) {
+    text("width: " + width, 30, 30);
+    text("height: " + height, 30, 45);
+    text("mouseX: " + mouseX, 30, 60);
+    text("mouseY: " + mouseY, 30, 75);
   }
 }
