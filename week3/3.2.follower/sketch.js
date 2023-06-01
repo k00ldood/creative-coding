4d0419be099d9274060fc3301d209711baad445b
k00ldood <bbcharlieca@gmail.com>
2023-05-31 21:21:08 -0400

    cookie monster

--- week3/3.2.follower/sketch.js
+++ week3/3.2.follower/sketch.js
@@ -1,46 +1,67 @@
-let speedfactor = 3;
-let xspeed = speedfactor;
-let yspeed = speedfactor;
+let debug = true;
 let x = 0;
 let y = 0;
+let d = 0;
+let score = 0;
+let speedfactor = 3;
+let speedx = speedfactor;
+let speedy = speedfactor;
 
-let debug = true;
-let followerImage;
 
-function preload() {
-  followerImage = loadImage("download.png");
+function setup(){
+  createCanvas(800,800);
+
+  x = random(width);
+  y = random(height);
+
 }
 
-function setup() {
-  createCanvas(800, 800);
-}
-
-function draw() {
+function draw(){
   background(220);
 
-  if (mouseX > x) {
-    xspeed = speedfactor;
-  } else {
-    xspeed = -speedfactor;
-  }
+// distance formula
+d = sqrt((x - mouseX)**2 + (y - mouseY)**2);
 
-  if (mouseY > y) {
-    yspeed = speedfactor;
-  } else {
-    yspeed = -speedfactor;
-  }
 
-  x += xspeed;
-  y += yspeed;
+// add the movement
+x += speedx;
+y += speedy;
 
-  let imgSize = 50;
-  imageMode(CENTER);
-  image(followerImage, x, y, imgSize, imgSize);
+//draw a circle for the follower
+circle(x,y,50);
 
-  if (debug) {
-    text("width: " + width, 30, 30);
-    text("height: " + height, 30, 45);
-    text("mouseX: " + mouseX, 30, 60);
-    text("mouseY: " + mouseY, 30, 75);
-  }
+if (mouseX > x){
+  //move to the right
+  speedx = speedfactor;
+}else{
+  //move to the left
+  speedx = -speedfactor;
+}
+
+if (mouseY > y){
+  //move to the right
+  speedy = speedfactor;
+}else{
+  //move to the left
+  speedy = -speedfactor;
+}
+
+//check for collision
+if (d < 25){
+  score -= 1;
+  x = random(width);
+  y = random(height);
+}
+
+if (debug){
+   textSize(30);
+   text("mouseX: " + mouseX, 50,50);
+   text("mouseY: " + mouseY, 50,80);
+   text("x: " + x, 50, 120);
+   text("y: " + y, 50, 150);
+   text("d: " + d, 50, 180);
+   text("score: " + score, 50, 210);
+
+}
+
 }
