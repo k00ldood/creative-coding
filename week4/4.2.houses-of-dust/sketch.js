1bd848acd90acb62fa4af63a223a789d1ed86c59
k00ldood <bbcharlieca@gmail.com>
2023-06-12 12:29:30 -0400

    HOUSE OF DUST

--- week4/4.2.houses-of-dust/sketch.js
+++ week4/4.2.houses-of-dust/sketch.js
@@ -1,7 +1,68 @@
+let poem;
+let grammar = tracery.createGrammar({
+  "origin": "A HOUSE OF #material#\n#place#\nUSING #light_source#\nINHABITED BY #inhabitants#",
+  "material": [
+    "Brick",
+    "Wood",
+    "Glass",
+    "Concrete",
+    "Stone",
+    "Steel",
+    "Clay",
+    "Straw",
+    "Bamboo",
+    "Adobe"
+  ],
+  "place": [
+    "Dreams",
+    "Whispers",
+    "Shadows",
+    "Solitude",
+    "Silence",
+    "Echoes",
+    "Memories",
+    "Forgotten Lore",
+    "Misty Moors",
+    "Time's Embrace"
+  ],
+  "light_source": [
+    "Candlelight",
+    "Sunlight",
+    "Moonlight",
+    "Starlight",
+    "Firelight",
+    "Lanterns",
+    "Neon Glow",
+    "Lighthouse Beam",
+    "Glowing Embers",
+    "Bioluminescence"
+  ],
+  "inhabitants": [
+    "Ghosts",
+    "Whispering Spirits",
+    "Wandering Souls",
+    "Lost Wanderers",
+    "Lingering Shadows",
+    "Silent Watchers",
+    "Ethereal Beings",
+    "Time Travelers",
+    "Cosmic Entities",
+    "Mystic Guardians"
+  ]
+});
+
+grammar.addModifiers(tracery.baseEngModifiers);
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(400, 400);
+  noLoop();
 }
 
 function draw() {
-  background(220);
+  poem = grammar.flatten("#origin#");
+
+  for (let i = 0; i < 4; i++) {
+    textSize(20);
+    text(poem, 10, 40 + i * 100);
+  }
 }
