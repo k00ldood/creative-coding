f45d85de14e9704b0cb3c48ea8b1b3e21a17a8ee
k00ldood <bbcharlieca@gmail.com>
2023-06-12 23:58:38 -0400

    final house of dust

--- week4/4.3.love-markov/sketch.js
+++ week4/4.3.love-markov/sketch.js
@@ -1,7 +1,26 @@
+let rm = RiTa.markov(2);
+
+let data = document.getElementById("source").innerText;
+rm.addText(data);
+
 function setup() {
-  createCanvas(800, 800);
+  let lines = rm.generate(6);
+  let letter = select("#letter");
+
+  letter.child(createP('My Dearest Harrington,'));
+
+  for (let l = 0; l < lines.length; l++) {
+    let paragraph = createP(lines[l]);
+    letter.child(paragraph);
+  }
+
+  letter.child(createP('Sincerely,<br>Markov'));
 }
 
-function draw() {
-  background(220);
+function mousePressed() {
+  let letter = select("#letter");
+  letter.remove();
+  letter = createDiv();
+  letter.id("letter");
+  setup();
 }
